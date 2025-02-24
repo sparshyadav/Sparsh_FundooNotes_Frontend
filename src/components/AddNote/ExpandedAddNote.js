def87f7a5e610acff6f939b1f7b2686e672fe57c
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-02-24 10:00:42 +0530

    [Sparsh] Refactor: AddNote Component Height, Margin Adjusted for Expandable State

--- src/components/AddNote/ExpandedAddNote.js
+++ src/components/AddNote/ExpandedAddNote.js
@@ -2,14 +2,14 @@ import React from 'react'
 import './ExpandedAddNote.scss';
 import { BellPlus, UserPlus, Image, FolderDown, EllipsisVertical } from 'lucide-react';
 
-function ExpandedAddNote({toggleView, setFunctions}) {
+function ExpandedAddNote({ toggleView, setFunctions }) {
     return (
         <div className='expended-main-container'>
             <div className='expended-title'>
-                <input className='title' type='text' placeholder='Title' onChange={(e)=>setFunctions.setTitle(e.target.value)} />
+                <input className='title' type='text' placeholder='Title' onChange={(e) => setFunctions.setTitle(e.target.value)} />
             </div>
             <div className='expended-note note'>
-                <input className='note' type='textarea' placeholder='Take a Note...' onChange={(e)=>setFunctions.setDescription(e.target.value)} />
+                <input className='note' type='textarea' placeholder='Take a Note...' onChange={(e) => setFunctions.setDescription(e.target.value)} />
             </div>
             <div className='expended-options'>
                 <div className='options-left'>
