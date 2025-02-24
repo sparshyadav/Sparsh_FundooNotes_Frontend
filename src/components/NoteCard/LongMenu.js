56f8dd835ea263a0486ba0704044c75cd13073a1
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-02-24 22:19:35 +0530

    [Sparsh] Refactor: Long Menu Options Loop Removed, and Options HardCoded

--- src/components/NoteCard/LongMenu.js
+++ src/components/NoteCard/LongMenu.js
@@ -1,73 +1,3 @@
-// import * as React from 'react';
-// import IconButton from '@mui/material/IconButton';
-// import Menu from '@mui/material/Menu';
-// import MenuItem from '@mui/material/MenuItem';
-// import MoreVertIcon from '@mui/icons-material/MoreVert';
-// import './LongMenu.scss'
-
-// const options = [
-//   'Delete Note',
-//   'Add Label',
-//   'Add Drawing',
-//   'Make a Copy',
-//   'Show Checkboxes',
-//   'Copy to Google Docs',
-//   'Version History'
-// ];
-
-// export default function LongMenu() {
-//   const [anchorEl, setAnchorEl] = React.useState(null);
-//   const open = Boolean(anchorEl);
-
-//   const handleClick = (event) => {
-//     setAnchorEl(event.currentTarget);
-//   };
-
-//   const handleClose = () => {
-//     setAnchorEl(null);
-//   };
-
-//   return (
-//     <div>
-//       <IconButton
-//         aria-label="more"
-//         id="long-button"
-//         aria-controls={open ? 'long-menu' : undefined}
-//         aria-expanded={open ? 'true' : undefined}
-//         aria-haspopup="true"
-//         onClick={handleClick}
-//       >
-//         <MoreVertIcon />
-//       </IconButton>
-//       <Menu
-//         id="long-menu"
-//         MenuListProps={{
-//           'aria-labelledby': 'long-button',
-//         }}
-//         anchorEl={anchorEl}
-//         open={open}
-//         onClose={handleClose}
-//         slotProps={{
-//           paper: {
-//             style: {
-//               width: '22ch',
-//               overflow: 'visible', 
-//             },
-//           },
-//         }}
-//       >
-//         {options.map((option) => (
-//           <MenuItem className='long-menu-item' key={option} onClick={handleClose}>
-//             {option}
-//           </MenuItem>
-//         ))}
-//       </Menu>
-//     </div>
-//   );
-// }
-
-
-
 import * as React from 'react';
 import IconButton from '@mui/material/IconButton';
 import Menu from '@mui/material/Menu';
@@ -75,17 +5,7 @@ import MenuItem from '@mui/material/MenuItem';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import './LongMenu.scss';
 
-const options = [
-  'Delete Note',
-  'Add Label',
-  'Add Drawing',
-  'Make a Copy',
-  'Show Checkboxes',
-  'Copy to Google Docs',
-  'Version History'
-];
-
-export default function LongMenu() {
+export default function LongMenu({ handleIconClick }) {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
 
@@ -117,13 +37,13 @@ export default function LongMenu() {
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
-        disableScrollLock={true} // Prevents the page from scrolling when the menu is open
+        disableScrollLock={true}
         anchorOrigin={{
-          vertical: 'bottom', // Fix it to the NoteCard
+          vertical: 'bottom',
           horizontal: 'right',
         }}
         transformOrigin={{
-          vertical: 'top', 
+          vertical: 'top',
           horizontal: 'right',
         }}
         slotProps={{
@@ -135,11 +55,16 @@ export default function LongMenu() {
           },
         }}
       >
-        {options.map((option) => (
-          <MenuItem className="long-menu-item" key={option} onClick={handleClose}>
-            {option}
-          </MenuItem>
-        ))}
+        <MenuItem className='long-menu-item' key={'Delete Note'} onClick={() => {
+          handleIconClick('trash')
+          handleClose();
+        }}>Delete a Note</MenuItem>
+        <MenuItem className='long-menu-item' key={'Add Label'} onClick={handleClose}>Add Label</MenuItem>
+        <MenuItem className='long-menu-item' key={'Add Drawing'} onClick={handleClose}>Add Drawing</MenuItem>
+        <MenuItem className='long-menu-item' key={'Make a Copy'} onClick={handleClose}>Make a Copy</MenuItem>
+        <MenuItem className='long-menu-item' key={'Show Checkboxes'} onClick={handleClose}>Show Checkboxes</MenuItem>
+        <MenuItem className='long-menu-item' key={'Copy to Google Docs'} onClick={handleClose}>Copy to Google Docs</MenuItem>
+        <MenuItem className='long-menu-item' key={'Version History'} onClick={handleClose}>Version History</MenuItem>
       </Menu>
     </div>
   );
