3aa6d9728073674e8ed7d649629c0cc7ee883b9f
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-02-25 16:39:02 +0530

    [Sparsh] Refactor: Dynamic Navbar Title Based on URL Path Implemented

--- src/components/DashboardContainer/Navbar.js
+++ src/components/DashboardContainer/Navbar.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React, { useEffect, useState } from 'react'
 import './Navbar.scss';
 import { Menu, RotateCw, Settings, Rows2, Grip, Search } from "lucide-react";
