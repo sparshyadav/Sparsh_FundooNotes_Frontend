5ea0c70eac7ebefeba37cc627899d714c70697e6
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-01 17:27:56 +0530

    [Sparsh] Refactor: FilteredNotes Used to Render Notes Instead of Notes

--- src/components/NotesContainer/NotesContainer.js
+++ src/components/NotesContainer/NotesContainer.js
@@ -1,66 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { getNotes } from '../../utils/API';
-import NoteCard from '../NoteCard/NoteCard';
-import './NotesContainer.scss'
-import AddNote from '../AddNote/AddNote';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import NoteCard from "../NoteCard/NoteCard";
+import "./NotesContainer.scss";
+import AddNote from "../AddNote/AddNote";
+import { useNotes } from "../../context/NotesContext";
 
 const NotesContainer = () => {
-    const [notes, setNotes] = useState([]);
-    // console.log(notes);
-
-    let noteId=useParams().noteId;
-    // console.log("Params: ", noteId);
-
-    useEffect(() => {
-        getNotes()
-            .then((response) => {
-                // console.log("Response: ==>>", response);
-                let normalNotes = (response?.data?.data?.data || []).filter((note) => {
-                    return (!note.isArchived && !note.isDeleted);
-                })
-                setNotes(normalNotes.reverse());
-
-                if (response.status !== 200) {
-                    throw new Error(response?.data?.message);
-                }
-            })
-            .catch((error) => {
-                console.error("Error fetching notes:", error);
-            });
-    }, []);
-
-    const updateNoteList = (response) => {
-        const { action, data } = response;
-
-        if (action === 'add') {
-            setNotes([{ title: data.title, description: data.description, id: data.id }, ...notes]);
-        }
-        else if (action === 'archive' || action === 'trash') {
-            setNotes(notes.filter((note) => {
-                return note.id !== data.id;
-            }))
-        }
-        else if (action === 'edit') {
-            setNotes(notes.map((note) => {
-                if (note.id === data.id) {
-                    return data;
-                }
-
-                return note;
-            }))
-        }
-    }
+    const { filteredNotes, updateNoteList } = useNotes();
 
     return (
-        <div className='notes-container'>
-            <div className='notes-addnote-component'>
+        <div className="notes-container">
+            <div className="notes-addnote-component">
                 <AddNote updateList={updateNoteList} />
             </div>
-            <div className='notes-main-container'>
-                {notes.map((note, index) => (
+            <div className="notes-main-container">
+                {filteredNotes.map((note, index) => (
                     <NoteCard
-                        style={{backgroundColor: note.color || 'white'}}
+                        style={{ backgroundColor: note.color || "white" }}
                         key={note.id || index}
                         title={note.title}
                         description={note.description}
