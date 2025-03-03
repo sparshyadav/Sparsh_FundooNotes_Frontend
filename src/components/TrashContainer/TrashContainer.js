fd812e45bc00411ba4683a04faeb6961653a0d25
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-03 18:32:35 +0530

    [Sparsh] Refactor: Remove Untrashed Note Cards Implemented

--- src/components/TrashContainer/TrashContainer.js
+++ src/components/TrashContainer/TrashContainer.js
@@ -1,54 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import { getNotes } from '../../utils/API';
-// import NoteCard from '../NoteCard/NoteCard';
-
-// const TrashContainer = () => {
-//   const [notes, setNotes] = useState([]);
-
-//   const fetchNotes = async () => {
-//     try {
-//       const response = await getNotes();
-//       let deletedNotes = (response?.data?.data?.data || []).filter((note) => note.isDeleted);
-//       setNotes(deletedNotes.reverse());
-//     } catch (error) {
-//       console.error("Error fetching notes:", error);
-//     }
-//   };
-
-//   useEffect(() => {
-//     fetchNotes();
-//   }, []);
-
-//   const updateNoteList = async (response) => {
-//     const { action, data } = response;
-
-//     if (action === 'trash') {
-//       setNotes(notes.filter((note) => {
-//         return note.id !== data.id;
-//       }))
-//     }
-//   };
-
-//   return (
-//     <div className='notes-main-container'>
-//       {notes.map((note, index) => (
-//         <NoteCard
-//           key={note.id || index}
-//           title={note.title}
-//           description={note.description}
-//           noteDetails={note}
-//           updateList={updateNoteList}
-//         />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default TrashContainer;
-
-
-
-
 import React, { useEffect, useState } from "react";
 import { getNotes } from "../../utils/API";
 import NoteCard from "../NoteCard/NoteCard";
@@ -74,7 +23,7 @@ const TrashContainer = () => {
   const updateNoteList = async (response) => {
     const { action, data } = response;
 
-    if (action === "trash") {
+    if (action === "untrash") {
       setNotes(notes.filter((note) => note.id !== data.id));
     }
   };
