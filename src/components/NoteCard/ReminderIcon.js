8537a5c59c61f50d4a56cbd25d9991679c65fd7e
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-06 14:40:39 +0530

    [Sparsh] Refactor: Handling Previous Date-Time for Reminder

--- src/components/NoteCard/ReminderIcon.js
+++ src/components/NoteCard/ReminderIcon.js
@@ -1,92 +1,3 @@
-// import * as React from 'react';
-// import Menu from '@mui/material/Menu';
-// import MenuItem from '@mui/material/MenuItem';
-// import './ReminderIcon.scss';
-// import { BellPlus } from 'lucide-react';
-// import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-// import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-// import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-// import { TimePicker } from '@mui/x-date-pickers/TimePicker';
-// import Button from '@mui/material/Button';
-
-// export default function LongMenu({ handleIconClick, noteDetails }) {
-//     const [date, setDate]=React.useState();
-//     const [time, setTime]=React.useState();
-//     const [anchorEl, setAnchorEl] = React.useState(null);
-//     const open = Boolean(anchorEl);
-
-//     const handleClick = (event) => {
-//         setAnchorEl(event.currentTarget);
-//     };
-
-//     const handleClose = () => {
-//         setAnchorEl(null);
-//     };
-
-//     return (
-//         <div>
-//             <BellPlus
-//                 className="icons"
-//                 onClick={handleClick}
-//                 aria-controls={open ? "long-menu" : undefined}
-//                 aria-haspopup="true"
-//             />
-//             <Menu
-//                 id="long-menu"
-//                 MenuListProps={{
-//                     'aria-labelledby': 'long-button',
-//                 }}
-//                 anchorEl={anchorEl}
-//                 open={open}
-//                 onClose={handleClose}
-//                 disableScrollLock={true}
-//                 anchorOrigin={{
-//                     vertical: 'bottom',
-//                     horizontal: 'right',
-//                 }}
-//                 transformOrigin={{
-//                     vertical: 'top',
-//                     horizontal: 'right',
-//                 }}
-//                 slotProps={{
-//                     paper: {
-//                         style: {
-//                             width: '300px',
-//                             overflow: 'visible',
-//                         },
-//                     },
-//                 }}
-//             >
-//                 <MenuItem className="long-menu-item" key="Add Label" >
-//                     <LocalizationProvider dateAdapter={AdapterDayjs}>
-//                         <DemoContainer components={['DatePicker']}>
-//                             <DatePicker label="Pick a Date" />
-//                         </DemoContainer>
-//                     </LocalizationProvider>
-//                 </MenuItem>
-//                 <MenuItem className="long-menu-item" key="Add Label">
-//                     <LocalizationProvider dateAdapter={AdapterDayjs}>
-//                         <DemoContainer components={['TimePicker']}>
-//                             <TimePicker label="Basic time picker" />
-//                         </DemoContainer>
-//                     </LocalizationProvider>
-//                 </MenuItem>
-//                 <div className='button-container'><Button variant="text" >Save</Button></div>
-//                 {/* <MenuItem className="long-menu-item" key="Add Drawing" onClick={handleClose}>Add Drawing</MenuItem>
-//                 <MenuItem className="long-menu-item" key="Make a Copy" onClick={handleClose}>Make a Copy</MenuItem>
-//                 <MenuItem className="long-menu-item" key="Show Checkboxes" onClick={handleClose}>Show Checkboxes</MenuItem>
-//                 <MenuItem className="long-menu-item" key="Copy to Google Docs" onClick={handleClose}>Copy to Google Docs</MenuItem>
-//                 <MenuItem className="long-menu-item" key="Version History" onClick={handleClose}>Version History</MenuItem> */}
-//             </Menu>
-//         </div>
-//     );
-// }
-
-
-
-
-
 import * as React from 'react';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
@@ -98,6 +9,8 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import Button from '@mui/material/Button';
+import dayjs from "dayjs";
+import { errorToast } from '../../utils/Toast';
 
 export default function LongMenu({ handleIconClick }) {
     const [date, setDate] = React.useState(null);
@@ -115,12 +28,17 @@ export default function LongMenu({ handleIconClick }) {
 
     const handleSave = () => {
         if (date && time) {
-            const formattedDate = date.format("YYYY-MM-DD");
-            const formattedTime = time.format("HH:mm:ss.SSS");
+            const selectedDateTime = dayjs(date.format("YYYY-MM-DD") + " " + time.format("HH:mm:ss"));
+            const currentDateTime = dayjs();
 
-            const dateTimeISO = `${formattedDate}T${formattedTime}Z`;
+            if (selectedDateTime.isBefore(currentDateTime)) {
+                errorToast("Please Select a Future Date");
+                return;
+            }
 
-            handleIconClick('reminder', dateTimeISO);
+            const dateTimeISO = selectedDateTime.toISOString();
+
+            handleIconClick("reminder", dateTimeISO);
         }
         handleClose();
     };
