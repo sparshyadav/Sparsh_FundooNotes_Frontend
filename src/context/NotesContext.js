8f738d305283a8bbe86048f1350cb5be8813f41c
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-06 14:17:12 +0530

    [Sparsh] Add: Functionality to add/remove Reminder

--- src/context/NotesContext.js
+++ src/context/NotesContext.js
@@ -30,7 +30,7 @@ export const NotesProvider = ({ children }) => {
     };
 
     const filteredNotes = notes.filter((note) => {
-        if (!searchedText.trim()) return true; 
+        if (!searchedText.trim()) return true;
         const searchText = searchedText.toLowerCase();
         return (
             (note?.title?.toLowerCase() || "").includes(searchText) ||
@@ -40,22 +40,30 @@ export const NotesProvider = ({ children }) => {
 
     const updateNoteList = (response) => {
         const { action, data } = response;
-        console.log("Action: ", action);
-        console.log("Data: ", data);
-        
+
         setNotes((prevNotes) => {
             if (action === "add") {
                 return [{ ...data }, ...prevNotes];
             } else if (action === "archive" || action === "trash") {
                 return prevNotes.filter((note) => note.id !== data.id);
+            } else if (action === "unarchive") {
+                return [{ ...data, isArchived: false }, ...prevNotes];  
             } else if (action === "edit") {
                 return prevNotes.map((note) => (note.id === data.id ? data : note));
+            } else if (action === "reminder") {
+                return prevNotes.map((note) =>
+                    note.id === data.id ? { ...note, reminder: [data.reminder] } : note
+                );
             }
-    
+            else if (action === 'removeReminder') {
+                return prevNotes.map((note) =>
+                    note.id === data.id ? { ...note } : note
+                );
+            }
+
             return prevNotes;
         });
     };
-    
 
     return (
         <NotesContext.Provider value={{ notes, setNotes, updateNoteList, searchedText, setSearchedText, filteredNotes }}>
