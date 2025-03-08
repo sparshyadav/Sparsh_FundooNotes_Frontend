5130969572ebedfce2264a4d4e163a49b2564220
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-08 15:18:47 +0530

    [Sparsh] Refactor: Toggle View State Functionality Implemented

--- src/components/DashboardContainer/Navbar.js
+++ src/components/DashboardContainer/Navbar.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from "react";
 import "./Navbar.scss";
-import { Menu, RotateCw, Settings, Rows2, Grip, Search } from "lucide-react";
+import { Menu, RotateCw, Settings, Rows2, Grip, Search, LayoutGrid } from "lucide-react";
 import AccountMenu from "./AccountMenu";
 import { useLocation } from "react-router-dom";
 import { useNotes } from "../../context/NotesContext";
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import { useChangeView } from "../../context/ViewChangeContext";
 
 function Navbar({ toggleSidebar }) {
     const [navTitle, setNavTitle] = useState("Keep");
     const { setSearchedText, searchedText } = useNotes();
     const location = useLocation();
     const path = location.pathname.split("/")[2];
+
+    const { view, setView } = useChangeView();
 
     useEffect(() => {
         if (path === "") setNavTitle("Keep");
@@ -29,7 +34,7 @@ function Navbar({ toggleSidebar }) {
                         <img
                             src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png"
                             alt="Image not Found"
-                            onClick={() => setSearchedText("")}/>
+                            onClick={() => setSearchedText("")} />
                     </div>
                     <div className="header-left-container-title" onClick={() => setSearchedText("")}>
                         {navTitle}
@@ -52,21 +57,50 @@ function Navbar({ toggleSidebar }) {
                 <div className="dashboard-header-right-container">
                     <div className="header-right-container-icons">
                         <div className="icon-div">
-                            <RotateCw className="header-icons" />
+                            <Tooltip title="Refresh">
+                                <IconButton>
+                                    <RotateCw className="header-icons" />
+                                </IconButton>
+                            </Tooltip>
+                            {/* <RotateCw className="header-icons" /> */}
                         </div>
                         <div className="icon-div row-icon">
-                            <Rows2 className="header-icons" />
+                            {
+                                view ?
+                                    <Tooltip title="List View">
+                                        <IconButton>
+                                            <Rows2 className="header-icons" onClick={() => setView((prev) => !prev)} />
+                                        </IconButton>
+                                    </Tooltip> :
+                                    <Tooltip title="Grid View">
+                                        <IconButton>
+                                            <LayoutGrid className="header-icons" onClick={() => setView((prev) => !prev)} />
+                                            {/* <Rows2 className="header-icons" onClick={() => setView((prev) => !prev)} /> */}
+                                        </IconButton>
+                                    </Tooltip>
+                            }
+                            {/* <Rows2 className="header-icons" /> */}
                         </div>
                         <div className="icon-div">
-                            <Settings className="header-icons" />
+                            <Tooltip title="Settings">
+                                <IconButton>
+                                    <Settings className="header-icons" />
+                                </IconButton>
+                            </Tooltip>
+                            {/* <Settings className="header-icons" /> */}
                         </div>
                     </div>
                     <div className="header-right-container-account">
                         <div className="icon-div-account grid-icon">
-                            <Grip className="header-icons grid" />
+                            <Tooltip title="Google Apps">
+                                <IconButton>
+                                    <Grip className="header-icons grid" />
+                                </IconButton>
+                            </Tooltip>
+                            {/* <Grip className="header-icons grid" /> */}
                         </div>
                         <div className="icon-div-account">
-                            <AccountMenu />
+                            <AccountMenu className="account-icon" />
                         </div>
                     </div>
                 </div>
