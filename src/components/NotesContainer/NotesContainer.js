4f7d8258bb533de9e68de4d175b903be2dab1d05
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-03-08 15:16:24 +0530

    [Sparsh] Refactor: Conditionally Rendering View Based on State

--- src/components/NotesContainer/NotesContainer.js
+++ src/components/NotesContainer/NotesContainer.js
@@ -4,9 +4,11 @@ import "./NotesContainer.scss";
 import AddNote from "../AddNote/AddNote";
 import { useNotes } from "../../context/NotesContext";
 import Masonry from "@mui/lab/Masonry";
+import { useChangeView } from "../../context/ViewChangeContext";
 
 const NotesContainer = () => {
     const { filteredNotes, updateNoteList } = useNotes();
+    const { view } = useChangeView();
 
     return (
         <div className="notes-container">
@@ -14,24 +16,41 @@ const NotesContainer = () => {
                 <AddNote updateList={updateNoteList} />
             </div>
             <div className="notes-main-container">
-                <Masonry
-                    columns={{ xs: 2, sm: 2, md: 3, lg: 4 }} 
-                    spacing={2}
-                >
-                    {filteredNotes.map((note, index) => (
-                        <NoteCard
-                            key={note.id || index}
-                            className="note-card"
-                            style={{
-                                backgroundColor: note.color || "white",
-                            }}
-                            title={note.title}
-                            description={note.description}
-                            noteDetails={note}
-                            updateList={updateNoteList}
-                        />
-                    ))}
-                </Masonry>
+                {
+                    view ?
+                        <Masonry
+                            columns={{ xs: 2, sm: 2, md: 3, lg: 4 }}
+                            spacing={2}
+                        >
+                            {filteredNotes.map((note, index) => (
+                                <NoteCard
+                                    key={note.id || index}
+                                    className="note-card"
+                                    style={{
+                                        backgroundColor: note.color || "white",
+                                    }}
+                                    title={note.title}
+                                    description={note.description}
+                                    noteDetails={note}
+                                    updateList={updateNoteList}
+                                />
+                            ))}
+                        </Masonry> :
+                        filteredNotes.map((note, index) => (
+                            <NoteCard
+                                key={note.id || index}
+                                className="note-card"
+                                style={{
+                                    backgroundColor: note.color || "white",
+                                }}
+                                title={note.title}
+                                description={note.description}
+                                noteDetails={note}
+                                updateList={updateNoteList}
+                            />
+                        ))
+
+                }
             </div>
         </div>
     );
